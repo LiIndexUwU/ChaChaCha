9528ba3d91dcbcbf7bb0f10885202df89f55631a
LiIndexUwU <93841328+LiIndexUwU@users.noreply.github.com>
2021-12-27 16:03:07 +0100

    Create AutoKill.java

---
+++ dev/LiIndex/ChachaClient/modules/combat/AutoKill.java
@@ -0,0 +1,25 @@
+package dev.LiIndex.ChachaClient.modules.combat;
+
+import net.minecraft.Minecraft;
+import math.minecraft.util.Hit;
+import math.minecraft.util.HitRange();
+import net.minecraft.server.Server;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.Player;
+import dev.LiIndex.ChachaClient.modules.Module;
+
+public class AutoKill {
+  public AutoKill() {
+    this.name = "KillAura";
+    this.Category = Category.COMBAT;
+  }
+  
+  public void onEnable() {
+    mc.getPlayer();
+    if (mc.player.isOnServer()) {
+      mc.getPlayer().getServer().getEntity().hit().inRange(100);
+    } else {
+      mc.getPlayer().getWorld().getEntity().hit().inRange(100);
+    }
+  }
+}
